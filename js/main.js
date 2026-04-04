3a7d1173eeadca4a2042264e34ad7c8b5288b8bb
Mikki667 <m_kirs@fanshaweonline.ca>
2026-04-03 22:48:30 -0400

    Update: JavaScript
    In this commit I added The Smooth Scroll that I had previously used in my assignments.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { MoviesApp } from './movie-app.js';
 import { HeaderMenu } from './header-menu.js';
 import { Animations } from './animations.js';
+import { SmoothScroll } from './smooth-scroll.js';
 
 MoviesApp();
 HeaderMenu();
 Animations();
+SmoothScroll();
